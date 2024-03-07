3e851dcb602b947457052a29c74e2e5b2dfa0de6
Daniel Gomez Villegas <daniel.gomezvillegas@cognizant.com>
2024-03-07 13:50:46 -0600

    adding retry mechanism to AuthenticationService

--- src/main/java/com/example/demo/config/security/Configuration.java
+++ src/main/java/com/example/demo/config/security/Configuration.java
@@ -2,8 +2,10 @@ package com.example.demo.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 
 @org.springframework.context.annotation.Configuration
+@EnableRetry
 public class Configuration {
 
     @Bean
--- src/main/java/com/example/demo/service/AuthenticationService.java
+++ src/main/java/com/example/demo/service/AuthenticationService.java
@@ -3,17 +3,25 @@ package com.example.demo.service;
 import com.example.demo.data.model.AuthenticationResponse;
 import com.example.demo.data.model.User;
 import com.example.demo.repository.UserRepository;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.example.demo.constant.SecurityConstant.USER_NOT_FOUND_AUTHENTICATING_MESSAGE;
 import static com.example.demo.constant.SecurityConstant.USER_SUCCESSFULLY_AUTHENTICATED_MESSAGE;
 
 @Service
 public class AuthenticationService {
+
+    private static final Logger LOGGER = Logger.getLogger(AuthenticationService.class.getName());
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -31,16 +39,31 @@ public class AuthenticationService {
         this.authenticationManager = authenticationManager;
     }
 
+    // TODO: normalize all the responses to be HttpResponse
+
+    @Retryable(retryFor = RuntimeException.class, maxAttempts = 3, backoff = @Backoff(delay = 3000))
     public AuthenticationResponse authenticate(User request) {
+        LOGGER.log(Level.INFO, "Authenticating user...");
+
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
             new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
-        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        try {
+            authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        } catch (AuthenticationException exception) {
+            LOGGER.log(Level.SEVERE, "User authentication failed", exception);
+        }
+
         User user = userRepository.findByUsername(request.getUsername())
             .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND_AUTHENTICATING_MESSAGE));
+
+        LOGGER.log(Level.INFO, "User authenticated successfully");
         return new AuthenticationResponse(USER_SUCCESSFULLY_AUTHENTICATED_MESSAGE, jwtService.generateToken(user));
     }
 
+    @Retryable(retryFor = RuntimeException.class, maxAttempts = 3, backoff = @Backoff(delay = 3000))
     public void register(User request) {
+        LOGGER.log(Level.INFO, "Registering user...");
         var user = User.builder()
             .username(request.getUsername())
             .password(passwordEncoder.encode(request.getPassword()))
@@ -51,5 +74,6 @@ public class AuthenticationService {
             .role(request.getRole())
             .build();
         userRepository.save(user);
+        LOGGER.log(Level.INFO, "User registered successfully");
     }
 }
