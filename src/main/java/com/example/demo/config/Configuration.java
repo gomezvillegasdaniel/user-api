ab93d741b916d0a1ad944a528ee8676e5cf27d5f
Daniel Gomez Villegas <daniel.gomezvillegas@cognizant.com>
2024-03-21 13:19:49 -0600

    moving configuration to its respective packages

--- src/main/java/com/example/demo/config/Configuration.java
+++ src/main/java/com/example/demo/config/Configuration.java
@@ -1,4 +1,4 @@
-package com.example.demo.config.security;
+package com.example.demo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
